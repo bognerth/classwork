6af012c254252ebb74a4bf9d1bde39e24b26a797
bognerth <bognerth@gmail.com>
2013-01-01 17:57:25 +0100

    edit

--- app/models/test_event.rb
+++ app/models/test_event.rb
@@ -6,7 +6,7 @@ class TestEvent < ActiveRecord::Base
   validates_inclusion_of :state, in: Test::STATES
 
   def self.with_last_state(state)
-    order("id desc").group("test_id").having(state: state)
+    order("id desc").group("test_id").where(state: state)
   end
 end
 
